165665e2928e1fe4018f2a9a13e2c608ad9093a6
jenZ313 <jinran.zhu@mail.utoronto.ca>
2021-11-09 18:48:17 -0500

    11

--- src/Command.java
+++ src/Command.java
@@ -1,35 +1,271 @@
-public interface Command {
-    public int execute();
+//These are gateways
+
+import java.sql.*;
+
+public class Command {
+    public final int SUCCESS = 0;
+    public final int FAILED = 1;
+    public final int USERNAMEALREADYUSED = 2;
+    public final int TEACHER = 11;
+    public final int STUDENT = 12;
+
+    public String driver = "com.mysql.cj.jdbc.Driver";//驱动程序名
+    public String url = "jdbc:MySQL://sql5.freemysqlhosting.net:3306/sql5449780";//url指向要访问的数据库study
+    public String user = "sql5449780";//MySQL配置时的用户名
+    public String password = "HNzHR6WEhn";//MySQL配置时的密码
+    public Connection connection = null;
+
+    public Command() {
+        try {
+            // 加载驱动类
+            Class.forName(driver);
+            // 建立连接
+            this.connection = DriverManager.getConnection(url,
+                    user, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int execute() {
+        return -1;
+    }
 }
 
-class loginStudentCommand implements Command {
-    private Student student;
+//String name,String pass -> STUDENT/TEACHER/FAILED
+class loginCommand extends Command {
     private String name;
     private String pass;
-    @Override
-    public int execute() {
 
-        return student.login(name, pass);
+    public loginCommand(String name, String pass) {
+        this.name = name;
+        this.pass = pass;
     }
-}
-class loginTeacherCommand implements Command {
-    @Override
-    public int execute() {
-        ;
-        return 11;
-    }
-}
-
-
-class quitGroupCommand implements Command {
 
     @Override
     public int execute() {
+        try {
+            //check if the user is a student
+            String tableName = "STUDENT";
+            Statement statement = connection.createStatement();
+            String sql = "select * from " + tableName + " where name='" + name + "' and pass='" + pass + "'";
+            ResultSet resultSet = statement.executeQuery(sql);
+            ////check if there is a match
+            boolean returnValue = resultSet.next();
+            statement.close();
+            connection.close();
+            if (returnValue) {
+                return STUDENT;
+            }
+
+            //check if the user is a teacher
+            tableName = "TEACHER";
+            statement = connection.createStatement();
+            sql = "select * from " + tableName + " where name='" + name + "' and pass='" + pass + "'";
+            resultSet = statement.executeQuery(sql);
+            ////check if there is a match
+            returnValue = resultSet.next();
+            statement.close();
+            connection.close();
+            if (returnValue) {
+                return TEACHER;
+            }
+
+            //if not both, return FAILED
+            return FAILED;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return FAILED;
+        }
+    }
+}
+
+//String name, String pass, String email, int type(teacher11, student12)-> USERNAMEALREADYUSED/SUCCESS/FAILED
+class registerCommand extends Command {
+    private String name;
+    private String pass;
+    private String email;
+    private int type;
+
+    public registerCommand(String name, String pass, String email, int type) {
+        this.name = name;
+        this.pass = pass;
+        this.email = email;
+        this.type = type;
+    }
+
+    @Override
+    public int execute() {
+        try {
+            //check if username already exists
+            ////check if name is used by a student
+            Statement statement = connection.createStatement();
+            String sql = "select * from STUDENT where name='" + name + "'";
+            ResultSet resultSet = statement.executeQuery(sql);
+            //////check if there is a match
+            boolean returnValue = resultSet.next();
+            if (returnValue) {
+                statement.close();
+                connection.close();
+                return USERNAMEALREADYUSED;
+            }
+            ////check if name is used by a teacher
+            sql = "select * from TEACHER where name='" + name + "'";
+            resultSet = statement.executeQuery(sql);
+            //////check if there is a match
+            returnValue = resultSet.next();
+            if (returnValue) {
+                statement.close();
+                connection.close();
+                return USERNAMEALREADYUSED;
+            }
+
+            //register
+            java.sql.Date sqlDate = new java.sql.Date(new java.util.Date().getTime());//get date
+            String groupID = "";//set groupID to []
+            ////register as a student:id(auto-generated)|name|pass|date|email|words|groupID|level
+            if (type == STUDENT) {
+                String words = "";//set words to []
+                int level = 1;//set level to 1
+                sql = "insert into student (name,pass,date,email,words,groupID,level) VALUE (?,?,?,?,?,?,?)";
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, pass);
+                preparedStatement.setDate(3, sqlDate);
+                preparedStatement.setString(4, email);
+                preparedStatement.setString(5, words);
+                preparedStatement.setString(6, groupID);
+                preparedStatement.setInt(7, level);
+                preparedStatement.executeUpdate();
+                statement.close();
+                connection.close();
+            }
+            ////register as a teacher:id|name|pass|date|email|groupID
+            else {
+                sql = "insert into student (name,pass,date,email,groupID) VALUE (?,?,?,?,?)";
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, pass);
+                preparedStatement.setDate(3, sqlDate);
+                preparedStatement.setString(4, email);
+                preparedStatement.setString(5, groupID);
+                preparedStatement.executeUpdate();
+                statement.close();
+                connection.close();
+            }
+            return SUCCESS;
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return FAILED;
+        }
+    }
+}
+
+//TODO
+//int studentID, int groupID -> SUCCESS/FAILED
+class quitGroupCommand extends Command {
+    int studentID;
+    int groupID;
+
+    public quitGroupCommand(int studentID, int groupID) {
+        this.studentID = studentID;
+        this.groupID = groupID;
+    }
+
+    @Override
+    public int execute() {
+        try {
+            //remove group from student
+            ////get all the groups joined by the student
+            String tableName = "STUDENT";
+            int groupIDCol = 7;
+            Statement statement = connection.createStatement();
+            String sql = "select * from " + tableName + " where id='" + studentID + "'";
+            ResultSet resultSet = statement.executeQuery(sql);
+            ////get all ids of groups joined by the student
+            resultSet.next();
+            String allGroups = resultSet.getString(groupIDCol);//in the form of "1,2,3,4"
+            ////remove groupID from the string
+            //TODO
+            ////rewrite the new string to the database
+            //TODO
+            //remove student from group
+            ////get all the students for the group
+            //TODO
+            ////remove studentID from the group
+            //TODO
+            statement.close();
+            connection.close();
+            return SUCCESS;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return FAILED;
+        }
+    }
+}
+
+//TODO
+//int studentID, int groupID -> SUCCESS/FAILED
+class joinGroupCommand extends Command {
+    int studentID;
+    int groupID;
+
+    public joinGroupCommand(int studentID, int groupID) {
+        this.studentID = studentID;
+        this.groupID = groupID;
+    }
+
+    @Override
+    public int execute() {
+        try {
+            //add group to student
+            ////get all the groups joined by the student
+            String tableName = "STUDENT";
+            int groupIDCol = 7;
+            Statement statement = connection.createStatement();
+            String sql = "select * from " + tableName + " where id='" + studentID + "'";
+            ResultSet resultSet = statement.executeQuery(sql);
+            ////get all ids of groups joined by the student
+            resultSet.next();
+            String allGroups = resultSet.getString(groupIDCol);//in the form of "1,2,3,4"
+            ////add groupID to the string
+            //TODO
+            ////rewrite the new string to the database
+            //TODO
+            //add student to group
+            ////get all the students for the group
+            //TODO
+            ////add studentID to the group
+            //TODO
+            statement.close();
+            connection.close();
+            return SUCCESS;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return FAILED;
+        }
+
 
     }
 }
 
-class joinCommand implements Command {
+//int teacherID, String groupName -> SUCCESS/FAILED
+class createGroupCommand extends Command {
+    String teacherName;
+    String groupName;
+    public createGroupCommand(String teacherName,String groupName){
+        this.teacherName=teacherName;
+        this.groupName=groupName;
+    }
+    @Override
+    public int execute() {
+
+    }
+}
+
+class deleteGroupCommand extends Command {
 
     @Override
     public int execute() {
@@ -37,7 +273,7 @@ class joinCommand implements Command {
     }
 }
 
-class registerstudentCommand implements Command {
+class changeNameCommand extends Command {
 
     @Override
     public int execute() {
@@ -45,7 +281,7 @@ class registerstudentCommand implements Command {
     }
 }
 
-class registerteacherCommand implements Command {
+class createTestCommand extends Command {
 
     @Override
     public int execute() {
@@ -53,7 +289,7 @@ class registerteacherCommand implements Command {
     }
 }
 
-class createGroupCommand implements Command {
+class classaddQuestationCommand extends Command {
 
     @Override
     public int execute() {
@@ -61,7 +297,7 @@ class createGroupCommand implements Command {
     }
 }
 
-class changeNameCommand implements Command {
+class releaseMarkCommand extends Command {
 
     @Override
     public int execute() {
@@ -69,7 +305,7 @@ class changeNameCommand implements Command {
     }
 }
 
-class createTestCommand implements Command {
+class inviteCommand extends Command {
 
     @Override
     public int execute() {
@@ -77,7 +313,7 @@ class createTestCommand implements Command {
     }
 }
 
-class addQuestationCommand implements Command {
+class deleteMemberCommand extends Command {
 
     @Override
     public int execute() {
@@ -85,7 +321,7 @@ class addQuestationCommand implements Command {
     }
 }
 
-class releaseMarkCommand implements Command {
+class createAnnouncementCommand extends Command {
 
     @Override
     public int execute() {
@@ -93,31 +329,7 @@ class releaseMarkCommand implements Command {
     }
 }
 
-class inviteCommand implements Command {
-
-    @Override
-    public int execute() {
-
-    }
-}
-
-class deleteMemberCommand implements Command {
-
-    @Override
-    public int execute() {
-
-    }
-}
-
-class createAnnouncementCommand implements Command {
-
-    @Override
-    public int execute() {
-
-    }
-}
-
-class submitCommand implements Command {
+class submitCommand extends Command {
 
     @Override
     public int execute() {
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,159 @@
+import java.sql.*;
+
 public class Main {
-    public static boolean initialTables(){
-
-        return false;
+    public static Connection getConnection() {
+        Connection conn = null;
+        String driver = "com.mysql.cj.jdbc.Driver";//驱动程序名
+        String url = "jdbc:MySQL://sql5.freemysqlhosting.net:3306/sql5449780";//url指向要访问的数据库study
+        String user = "sql5449780";//MySQL配置时的用户名
+        String password = "HNzHR6WEhn";//MySQL配置时的密码
+        try {
+            // 加载驱动类
+            Class.forName(driver);
+            // 建立连接
+            conn = DriverManager.getConnection(url,
+                    user, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return conn;
     }
-    public static void main(String[] args){
 
+    public static boolean ini() {
+        return iniGroupTable() && iniTestTable() && iniWordTable() && iniQuestionTable() && iniStudentTable() && iniTeacherTable();
+
+    }
+
+    public static boolean iniStudentTable() {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS STUDENT " +
+                    "(id INT UNSIGNED AUTO_INCREMENT, " +
+                    " name VARCHAR(16), " +
+                    " pass VARCHAR(16), " +
+                    " date Date, " +
+                    " email VARCHAR(255), " +
+                    " words VARCHAR(1000), " +
+                    " groupID VARCHAR(1000), " +
+                    " level INT, " +
+                    " PRIMARY KEY ( id ))";
+            statement.executeUpdate(sql);
+            System.out.println("Created student table in given database...");
+            statement.close();
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public static boolean iniTeacherTable() {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS TEACHER " +
+                    "(id INT UNSIGNED AUTO_INCREMENT, " +
+                    " name VARCHAR(16), " +
+                    " pass VARCHAR(16), " +
+                    " date Date, " +
+                    " email VARCHAR(255), " +
+                    " groupID VARCHAR(1000), " +
+                    " PRIMARY KEY ( id ))";
+            statement.executeUpdate(sql);
+            System.out.println("Created teacher table in given database...");
+            statement.close();
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public static boolean iniGroupTable() {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS STUDYGROUP " +
+                    "(id INT UNSIGNED AUTO_INCREMENT, " +
+                    " name VARCHAR(16), " +
+                    " creator VARCHAR(16), " +
+                    " students VARCHAR(1000), " +
+                    " posts VARCHAR(1000), " +
+                    " PRIMARY KEY ( id ))";
+            statement.executeUpdate(sql);
+            System.out.println("Created group table in given database...");
+            statement.close();
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public static boolean iniWordTable() {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS WORD " +
+                    "(id INT UNSIGNED AUTO_INCREMENT, " +
+                    " spelling VARCHAR(255), " +
+                    " meaning VARCHAR(255), " +
+                    " level INT, " +
+                    " PRIMARY KEY ( id )) CHARACTER SET = utf8";
+            statement.executeUpdate(sql);
+            System.out.println("Created student table in given database...");
+            statement.close();
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public static boolean iniTestTable() {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS TEST " +
+                    "(id INT UNSIGNED AUTO_INCREMENT, " +
+                    " name VARCHAR(16), " +
+                    " author VARCHAR(16), " +
+                    " time INT, " +
+                    " price INT, " +
+                    " questions VARCHAR(1000), " +
+                    " PRIMARY KEY ( id )) CHARACTER SET = utf8";
+            statement.executeUpdate(sql);
+            System.out.println("Created test table in given database...");
+            statement.close();
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public static boolean iniQuestionTable() {
+        try {
+            Connection connection = getConnection();
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS QUESTION " +
+                    "(id INT UNSIGNED AUTO_INCREMENT, " +
+                    " question VARCHAR(1000), " +
+                    " answer VARCHAR(1000), " +
+                    " PRIMARY KEY ( id )) CHARACTER SET = utf8";
+            statement.executeUpdate(sql);
+            System.out.println("Created question table in given database...");
+            statement.close();
+            connection.close();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
     }
 
 }
