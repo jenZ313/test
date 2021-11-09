198c65841c2be582966d6706c8e098e4961d6b2b
jenZ313 <jinran.zhu@mail.utoronto.ca>
2021-11-09 01:14:26 -0500

    11

---
+++ src/Command.java
@@ -0,0 +1,107 @@
+public interface Command {
+    public void execute();
+}
+
+class loginCommand implements Command {
+    @Override
+    public void execute() {
+
+    }
+}
+
+class quitGroupCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
+class joinCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
+class registerCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
+class createGroupCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
+class changeNameCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
+class createTestCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
+class addQuestationCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
+class releaseMarkCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
+class inviteCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
+class deleteMemberCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
+class createAnnouncementCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
+class submitCommand implements Command {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
