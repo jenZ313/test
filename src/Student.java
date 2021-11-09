217036b92b47ab977472e243bf28051508b8e557
jenZ313 <jinran.zhu@mail.utoronto.ca>
2021-11-09 00:27:23 -0500

    Initial commit

---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    String name;
+    String pass;
+    public Student(String s1, String s2){
+        name=s1;
+        pass=s2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+}
