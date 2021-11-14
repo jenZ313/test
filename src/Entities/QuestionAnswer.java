39a23054d3162dab02b2ade7e8b2148e7345af4b
jacky <jacky.jiang@mail.utoronto.ca>
2021-11-13 21:29:55 -0500

    21.29

---
+++ src/Entities/QuestionAnswer.java
@@ -0,0 +1,6 @@
+public class QuestionAnswer {
+    private int questionID;
+    private String answer;
+    private int mark;
+    private int studentID;
+}
---
+++ src/Entities/Student.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Student {
+    private String pass;
+    private String name;
+    private String words;
+    private String email;
+    private int[] testID;
+    private int[] answerID;
+    private int[] groupID;
+    private int level;
+    private Date date;
+
+}
